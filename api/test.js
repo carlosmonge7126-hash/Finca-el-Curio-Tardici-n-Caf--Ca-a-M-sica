7e3e1a27c16578b32067034bc886f63761fdedbc
carlosmonge7126-hash <carlos.monge7126@gmail.com>
2026-06-25 10:31:51 -0600

    Delete api directory

--- api/test.js
+++
@@ -1,8 +0,0 @@
-// api/test.js
-module.exports = function(req, res) {
-  res.status(200).json({ 
-    mensaje: "✅ API funcionando correctamente",
-    version: "1.0.0",
-    timestamp: new Date().toISOString()
-  });
-};
