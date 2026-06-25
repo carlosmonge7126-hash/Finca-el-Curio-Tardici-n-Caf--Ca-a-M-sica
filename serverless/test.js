8c9be08bb9cc11822122e8af10078f15132ef5f4
carlosmonge7126-hash <carlos.monge7126@gmail.com>
2026-06-25 10:39:38 -0600

    Update and rename .gitkeep to test.js

---
+++ serverless/test.js
@@ -0,0 +1,8 @@
+// serverless/test.js
+module.exports = function(req, res) {
+  res.status(200).json({ 
+    mensaje: "✅ API funcionando correctamente",
+    version: "1.0.0",
+    timestamp: new Date().toISOString()
+  });
+};
